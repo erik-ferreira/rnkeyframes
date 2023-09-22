30b2056e812e677f7534070121bbf2f0501042dc
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-22 16:55:14 -0300

    add animation with keyframes

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,14 +1,48 @@
 import { Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
-import Animated from "react-native-reanimated"
+import Animated, { Keyframe } from "react-native-reanimated"
 
 import { styles } from "./styles"
 
 interface MessageProps {}
 
 export function Message({ ...rest }: MessageProps) {
+  // 1º forma de trabalhar com keyframes
+  const enteringKeyFrame = new Keyframe({
+    0: {
+      opacity: 0,
+      transform: [{ translateY: -100 }],
+    },
+
+    70: {
+      opacity: 0.3,
+    },
+
+    100: {
+      opacity: 1,
+      transform: [{ translateY: 0 }],
+    },
+  })
+
+  // 2º forma de trabalhar com keyframes
+  const exitingKeyFrame = new Keyframe({
+    from: {
+      opacity: 1,
+      transform: [{ translateY: 0 }],
+    },
+
+    to: {
+      opacity: 0,
+      transform: [{ translateY: -100 }],
+    },
+  })
+
   return (
-    <Animated.View style={styles.container}>
+    <Animated.View
+      style={styles.container}
+      entering={enteringKeyFrame.duration(500)}
+      exiting={exitingKeyFrame.duration(500)}
+    >
       <MaterialIcons name="notifications" color="#FFF" size={18} />
       <Text style={styles.title}>Mensagem de exemplo</Text>
     </Animated.View>
